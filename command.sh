9dd9ff8580b3d712985605a30d9af907b4ea1c3e
Jinhyung <kkjh0723@gmail.com>
2018-08-11 00:24:06 +0900

    Update command.sh

--- command.sh
+++ command.sh
@@ -3,13 +3,13 @@
 
 #
 #python train.py -a=wideresnet -m=baseline -o=adam -b=225 --dataset=cifar10 --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=pi -o=adam -b=225 --dataset=cifar10 --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=mt -o=adam -b=225 --dataset=cifar10 --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=pi -o=adam -b=225 --dataset=cifar10 --ckpt=ckpt --gpu=0,1 --lr=0.0003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=mt -o=adam -b=225 --dataset=cifar10 --ckpt=ckpt --gpu=0,1 --lr=0.0004 --boundary=0 --epochs=1200 &&
 #python train.py -a=wideresnet -m=baseline -o=adam -b=225 --dataset=cifar10_zca --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=pi -o=adam -b=225 --dataset=cifar10_zca --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=mt -o=adam -b=225 --dataset=cifar10_zca --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=pi -o=adam -b=225 --dataset=cifar10_zca --ckpt=ckpt --gpu=0,1 --lr=0.0003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=mt -o=adam -b=225 --dataset=cifar10_zca --ckpt=ckpt --gpu=0,1 --lr=0.0004 --boundary=0 --epochs=1200 &&
 #python train.py -a=wideresnet -m=baseline -o=adam -b=198 --dataset=svhn --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=pi -o=adam -b=198 --dataset=svhn --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
-#python train.py -a=wideresnet -m=mt -o=adam -b=198 --dataset=svhn --ckpt=ckpt --gpu=0,1 --lr=0.003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=pi -o=adam -b=198 --dataset=svhn --ckpt=ckpt --gpu=0,1 --lr=0.0003 --boundary=0 --epochs=1200 &&
+#python train.py -a=wideresnet -m=mt -o=adam -b=198 --dataset=svhn --ckpt=ckpt --gpu=0,1 --lr=0.0004 --boundary=0 --epochs=1200 &&
 
 ls
