5ee271924d8e80fc23cfa37038eef3cfae68cf80
Jinhyung Kim <kkjh0723@gmail.com>
2018-08-15 13:24:10 +0900

    check result plot all ckpt

--- check_result.py
+++ check_result.py
@@ -30,6 +30,35 @@ for i in range(nckpt):
     print('Val. Error: ',best_prec1_val)
     best_prec1s.append(best_prec1)
 
+    fname_acc = os.path.join(fdir,'accuracy%d.png'%i)
+    fname_loss = os.path.join(fdir,'losses_%d.png'%i)
+    acc1_tr = checkpoint['acc1_tr']
+    acc1_val = checkpoint['acc1_val']
+    acc1_te = checkpoint['acc1_test']
+    losses_tr = checkpoint['losses_tr']
+    losses_val = checkpoint['losses_val']
+    losses_te = checkpoint['losses_test']
+
+    if(args.plot):
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        ax = plt.subplot(2,1,1) 
+        ax.plot(acc1_tr, label='train_acc1')
+        ax.plot(acc1_val, label='val_acc1')
+        ax.plot(acc1_te, label='test_acc1')
+        ax.legend()
+        ax.grid(linestyle='--')
+        ax = plt.subplot(2,1,2) 
+        ax.plot(losses_tr, label='train_loss')
+        ax.plot(losses_val, label='val_loss')
+        ax.plot(losses_te, label='test_loss')
+        ax.legend()
+        ax.grid(linestyle='--')
+        plt.savefig(fname_acc)
+        #plt.show()
+        plt.clf()
+
+    #plt.show()
 best_prec1s = np.array(best_prec1s)
 bmean = np.around(np.mean(best_prec1s), decimals=2)
 bstd = np.around(np.std(best_prec1s), decimals=2)
