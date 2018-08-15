aad421bfb023c4d81c4b27a9b7f9c151f8f7e884
kkjh0723 <kkjh0723@gmail.com>
2018-08-15 13:25:52 +0900

    .

--- check_result.py
+++ check_result.py
@@ -68,32 +68,4 @@ print('Best error rate: %.2f(%.2f)'%(bmean,bstd))
 #for key, val in checkpoint.iteritems():
 #  print(key)
 
-fname_acc = os.path.join(fdir,'accuracy.png')
-fname_loss = os.path.join(fdir,'losses.png')
-acc1_tr = checkpoint['acc1_tr']
-acc1_val = checkpoint['acc1_val']
-acc1_te = checkpoint['acc1_test']
-losses_tr = checkpoint['losses_tr']
-losses_val = checkpoint['losses_val']
-losses_te = checkpoint['losses_test']
 
-if(args.plot):
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = plt.subplot(2,1,1) 
-    ax.plot(acc1_tr, label='train_acc1')
-    ax.plot(acc1_val, label='val_acc1')
-    ax.plot(acc1_te, label='test_acc1')
-    ax.legend()
-    ax.grid(linestyle='--')
-    ax = plt.subplot(2,1,2) 
-    ax.plot(losses_tr, label='train_loss')
-    ax.plot(losses_val, label='val_loss')
-    ax.plot(losses_te, label='test_loss')
-    ax.legend()
-    ax.grid(linestyle='--')
-    plt.savefig(fname_acc)
-    #plt.show()
-    plt.clf()
-
-    #plt.show()
